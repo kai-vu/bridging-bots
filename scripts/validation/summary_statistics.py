a20c5d2e79654685f572952b918694f5c3a73761
Margherita Martorana <m.martorana@vu.nl>
2025-05-20 22:47:30 +0200

    ..

--- scripts/validation/summary_statistics.py
+++ scripts/validation/summary_statistics.py
@@ -104,55 +104,55 @@ if __name__ == '__main__':
 
     files_observation_graph = [
         # llava-llama3
-        "../../output/llava-llama3/observation-graph/dynamicKG/kg.ttl",
-        "../../output/llava-llama3/observation-graph/imageToKG/kg.ttl",
-        "../../output/llava-llama3/observation-graph/promptKG/kg.ttl",
-        "../../output/llava-llama3/observation-graph/ragKG/kg.ttl",
+        "../../output/llava-llama3/observation-graph/dpe/kg.ttl",
+        "../../output/llava-llama3/observation-graph/i2kg/kg.ttl",
+        "../../output/llava-llama3/observation-graph/d2kg/kg.ttl",
+        "../../output/llava-llama3/observation-graph/d2kg-rag/kg.ttl",
         # llama4-scout
-        "../../output/llama4-scout/observation-graph/dynamicKG/kg.ttl",
-        "../../output/llama4-scout/observation-graph/imageToKG/kg.ttl",
-        "../../output/llama4-scout/observation-graph/promptKG/kg.ttl",
-        "../../output/llama4-scout/observation-graph/ragKG/kg.ttl",
+        "../../output/llama4-scout/observation-graph/dpe/kg.ttl",
+        "../../output/llama4-scout/observation-graph/i2kg/kg.ttl",
+        "../../output/llama4-scout/observation-graph/d2kg/kg.ttl",
+        "../../output/llama4-scout/observation-graph/d2kg-rag/kg.ttl",
         # llama4-maverick
-        "../../output/llama4-maverick/observation-graph/dynamicKG/kg.ttl",
-        "../../output/llama4-maverick/observation-graph/imageToKG/kg.ttl",
-        "../../output/llama4-maverick/observation-graph/promptKG/kg.ttl",
-        "../../output/llama4-maverick/observation-graph/ragKG/kg.ttl",
+        "../../output/llama4-maverick/observation-graph/dpe/kg.ttl",
+        "../../output/llama4-maverick/observation-graph/i2kg/kg.ttl",
+        "../../output/llama4-maverick/observation-graph/d2kg/kg.ttl",
+        "../../output/llama4-maverick/observation-graph/d2kg-rag/kg.ttl",
         # gpt-o1
-        "../../output/gpt-o1/observation-graph/imageToKG/kg.ttl",
-        "../../output/gpt-o1/observation-graph/promptKG/kg.ttl",
-        "../../output/gpt-o1/observation-graph/ragKG/kg.ttl",
+        "../../output/gpt-o1/observation-graph/i2kg/kg.ttl",
+        "../../output/gpt-o1/observation-graph/d2kg/kg.ttl",
+        "../../output/gpt-o1/observation-graph/d2kg-rag/kg.ttl",
         # gpt-4.1-nano
-        "../../output/gpt-4.1-nano/observation-graph/imageToKG/kg.ttl",
-        "../../output/gpt-4.1-nano/observation-graph/promptKG/kg.ttl",
-        "../../output/gpt-4.1-nano/observation-graph/ragKG/kg.ttl",
+        "../../output/gpt-4.1-nano/observation-graph/i2kg/kg.ttl",
+        "../../output/gpt-4.1-nano/observation-graph/d2kg/kg.ttl",
+        "../../output/gpt-4.1-nano/observation-graph/d2kg-rag/kg.ttl",
     ]
     ontology_observation_graph = "../../ontology/ontoObservationGraph.ttl"
 
     files_action_graph = [
         # llava-llama3
-        "../../output/llava-llama3/action-graph/dynamicKG/kg.ttl",
-        "../../output/llava-llama3/action-graph/imageToKG/kg.ttl",
-        "../../output/llava-llama3/action-graph/promptKG/kg.ttl",
-        "../../output/llava-llama3/action-graph/ragKG/kg.ttl",
+        "../../output/llava-llama3/action-graph/dpe/kg.ttl",
+        "../../output/llava-llama3/action-graph/i2kg/kg.ttl",
+        "../../output/llava-llama3/action-graph/d2kg/kg.ttl",
+        "../../output/llava-llama3/action-graph/d2kg-rag/kg.ttl",
         # llama4-scout
-        "../../output/llama4-scout/action-graph/dynamicKG/kg.ttl",
-        "../../output/llama4-scout/action-graph/imageToKG/kg.ttl",
-        "../../output/llama4-scout/action-graph/promptKG/kg.ttl",
-        "../../output/llama4-scout/action-graph/ragKG/kg.ttl",
+        "../../output/llama4-scout/action-graph/dpe/kg.ttl",
+        "../../output/llama4-scout/action-graph/i2kg/kg.ttl",
+        "../../output/llama4-scout/action-graph/d2kg/kg.ttl",
+        "../../output/llama4-scout/action-graph/d2kg-rag/kg.ttl",
         # llama4-maverick
-        "../../output/llama4-maverick/action-graph/dynamicKG/kg.ttl",
-        "../../output/llama4-maverick/action-graph/imageToKG/kg.ttl",
-        "../../output/llama4-maverick/action-graph/promptKG/kg.ttl",
-        "../../output/llama4-maverick/action-graph/ragKG/kg.ttl",
+        "../../output/llama4-maverick/action-graph/dpe/kg.ttl",
+        "../../output/llama4-maverick/action-graph/i2kg/kg.ttl",
+        "../../output/llama4-maverick/action-graph/d2kg/kg.ttl",
+        "../../output/llama4-maverick/action-graph/d2kg-rag/kg.ttl",
         # gpt-o1
-        "../../output/gpt-o1/action-graph/imageToKG/kg.ttl",
-        "../../output/gpt-o1/action-graph/promptKG/kg.ttl",
-        "../../output/gpt-o1/action-graph/ragKG/kg.ttl",
+        "../../output/gpt-o1/action-graph/i2kg/kg.ttl",
+        "../../output/gpt-o1/action-graph/d2kg/kg.ttl",
+        "../../output/gpt-o1/action-graph/d2kg-rag/kg.ttl",
         # gpt-4.1-nano
-        "../../output/gpt-4.1-nano/action-graph/imageToKG/kg.ttl",
-        "../../output/gpt-4.1-nano/action-graph/promptKG/kg.ttl",
-        "../../output/gpt-4.1-nano/action-graph/ragKG/kg.ttl",
+        "../../output/gpt-4.1-nano/action-graph/i2kg/kg.ttl",
+        "../../output/gpt-4.1-nano/action-graph/d2kg/kg.ttl",
+        "../../output/gpt-4.1-nano/action-graph/d2kg-rag/kg.ttl",
     ]
     ontology_action_graph = "../../ontology/ontoActionGraph.ttl"
 
